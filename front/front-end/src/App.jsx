7cfc696f4461e79ce0b1de0668937f9b59357b50
salimBenSlim1 <salimbenslim09@gmail.com>
2023-12-15 15:22:11 +0100

    done

--- front/front-end/src/App.jsx
+++ front/front-end/src/App.jsx
@@ -2,12 +2,14 @@ import Login from './components/Login.jsx'
 import React, { useState } from 'react'
 import { Routes,Route, BrowserRouter, useSearchParams } from 'react-router-dom'
 import User from './components/User.jsx'
+import NoPage from './components/NoPage.jsx'
  function App() {
   const[token,setToken]=useState('')
   return (
 <Routes>
   <Route path='/' element={<Login token={token} setToken={setToken}/>}/>
   <Route path='/user' element={<User token={token}/>}/>
+  <Route path="*" element={<NoPage />} />
 </Routes>
 
   )
